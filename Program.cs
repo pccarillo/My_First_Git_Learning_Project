ec338c24a882ba262cb3aad96d8475a8a80fb58b
Pietro <pietro.carillo@gmail.com>
2020-01-21 17:47:21 -0500

    This is a new change to display the second message

--- Program.cs
+++ Program.cs
@@ -6,7 +6,11 @@ namespace My_First_Git_Learning_Project
     {
         static void Main(string[] args)
         {
+            // Display the "Hello World!" message!
             Console.WriteLine("Hello World!");
+
+            // Display the second message of ISM 4300
+            Console.WriteLine("This is ISM 4300!");
         }
     }
 }
