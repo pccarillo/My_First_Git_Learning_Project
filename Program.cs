4323fa8b370015cd4d693149f43428c52cebc6c3
Pietro <pietro.carillo@gmail.com>
2020-01-21 17:24:28 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace My_First_Git_Learning_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
