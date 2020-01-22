6f025ba8f2d76ca69e37efe8d4dea750d27a3021
Pietro <pietro.carillo@gmail.com>
2020-01-22 11:01:57 -0500

    Commit all

--- Program.cs
+++ Program.cs
@@ -6,12 +6,9 @@ namespace My_First_Git_Learning_Project
     {
         static void Main(string[] args)
         {
-            // Display the "Hello World!" message!
+            // Pause to display Hello World
+            Console.WriteLine("Press any key to continue...");
             Console.WriteLine("Hello World!");
-
-            // Display the second message of ISM 4300
-            Console.WriteLine("This is ISM 4300!");
-
             Console.ReadKey(true);
         }
     }
